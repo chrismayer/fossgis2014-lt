bc2a37cb623b6f8723a55858a29af790c3169fb4
Christian Mayer <christian.mayer@isb-ag.de>
2014-03-18 14:55:41 +0100

    Added a reload button to ol3 example
    Implemented a button for the ol3 demo application which reloads the demo
    WMS-layer (bands) in order to refresh the layer without loading the app
    again.

--- resources/ol3-wms-demo/ol3-wms-demo.js
+++ resources/ol3-wms-demo/ol3-wms-demo.js
@@ -1,38 +1,60 @@
+// the ol3 map object
+var map;
+
+/**
+ * initializes the map and its layers
+ */
 function init() {
 
-var layers = [
-	new ol.layer.Tile({
-		source: new ol.source.TileWMS({
-			url: 'http://ows.terrestris.de/osm-gray/service',
-			params: {
-				LAYERS: 'OSM-WMS',
-				VERSION: '1.1.1'
-			}
-		})
-	}),
-	new ol.layer.Tile({
-		source: new ol.source.TileWMS({
-			url: 'http://localhost:10080/geoserver243/fossgis2014/wms',
-			params: {
-				LAYERS: 'bands',
-				VERSION: '1.1.1'
-			}
-		})
-	})];
+    var layers = [
+        new ol.layer.Tile({
+            source: new ol.source.TileWMS({
+                url: 'http://ows.terrestris.de/osm-gray/service',
+                params: {
+                    LAYERS: 'OSM-WMS',
+                    VERSION: '1.1.1'
+                }
+            })
+        }),
+        new ol.layer.Tile({
+            source: new ol.source.TileWMS({
+                url: 'http://localhost:10080/geoserver243/fossgis2014/wms',
+                params: {
+                    LAYERS: 'bands',
+                    VERSION: '1.1.1'
+                }
+            })
+        })
+    ];
 
     var projection = new ol.proj.Projection({
         code: 'EPSG:900913',
         units: 'm'
       });
     
-    var map = new ol.Map({
+    map = new ol.Map({
         layers: layers,
         renderer: [/*"webgl",*/ "canvas", "dom"],
         target: 'map',
         view: new ol.View2D({
-          center: [0, 0],
-          projection: projection,
-          zoom: 2
+            center: [0, 0],
+            projection: projection,
+            zoom: 2
         })
-      });
+    });
+}
+
+/**
+ * Redraws the demo WMS-layer (bands)
+ */
+function redrawBandLayer() {
+    var layers = map.getLayers();
+    // iterate over ol3 collection
+    layers.forEach(function(layer, idx, arr) {
+        var layerSource = layer.getSource(),
+            wmsParams = layerSource.getParams();
+        if (wmsParams.LAYERS.indexOf("bands") !== -1) {
+            layerSource.updateParams({rand: Math.random()});
+        }
+    });
 }
